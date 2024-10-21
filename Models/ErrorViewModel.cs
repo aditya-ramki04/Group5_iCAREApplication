e17829267c8e331a1ce1dcd5bbf9aad439988728
Aditya Ramakrishnan <112342054+aditya-ramki04@users.noreply.github.com>
2024-10-20 20:08:14 -0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace iCareWebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
