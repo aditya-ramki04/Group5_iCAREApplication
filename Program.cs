8280742eb668a6c3600d8f6032f2acac94834d92
Aditya Ramakrishnan <112342054+aditya-ramki04@users.noreply.github.com>
2024-10-21 20:24:37 -0500

    Added iCareContext to Program.cs entry

--- Program.cs
+++ Program.cs
@@ -1,16 +1,26 @@
+using iCareWebApplication.Data; // Import your data namespace where iCareContext is located
+using iCareWebApplication.Data;
+using Microsoft.EntityFrameworkCore; // Import EF Core namespace
+using Pomelo.EntityFrameworkCore.MySql.Infrastructure; // Import MySQL specific infrastructure
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+// Configure MySQL Database Connection
+builder.Services.AddDbContext<iCareContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("MySqlConnection"),
+    new MySqlServerVersion(new Version(8, 0, 21)) // Specify your MySQL version
+));
+
+// Build the app
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
