3f24fe0cf73fcc81a26883baf4091e97a672c5d4
Aditya Ramakrishnan <112342054+aditya-ramki04@users.noreply.github.com>
2024-10-21 19:43:32 -0500

    Added connection string to appsettings.json

---
+++ Data/iCareContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Reflection.Metadata;
+
+namespace iCareWebApplication.Data
+{
+    public class iCareContext : DbContext
+    {
+        public iCareContext(DbContextOptions<iCareContext> options) : base(options) { }
+
+        // Define DbSet for each entity/table in the database
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<PatientAssignment> PatientAssignments { get; set; }
+        public DbSet<TreatmentRecord> TreatmentRecords { get; set; }
+        public DbSet<Document> Documents { get; set; }
+        public DbSet<ModificationHistory> ModificationHistories { get; set; }
+        public DbSet<Drug> Drugs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Optional: Configure entity relationships here
+        }
+    }
+}
