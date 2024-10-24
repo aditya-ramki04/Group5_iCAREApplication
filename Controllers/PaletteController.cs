a5da68e1d8ad0a0f977bff87ff9361f272fca431
mattrobi04 <mattrobi04@gmail.com>
2024-10-24 16:40:00 -0500

    creating view and controller for pallete and patient

---
+++ Controllers/PaletteController.cs
@@ -0,0 +1,25 @@
+﻿using iCareWebApplication.Data;
+using iCareWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace iCareWebApplication.Controllers
+{
+    public class PaletteController : Controller // Ensure this is RoleController and it inherits from Controller
+    {
+        private readonly iCareContext _context;
+
+        public PaletteController(iCareContext context)
+        {
+            _context = context;
+        }
+
+        // GET: /Role/Index
+        public async Task<IActionResult> Index()
+        {
+            var roles = await _context.iCareDocuments.ToListAsync();
+            return View(roles);
+        }
+    }
+}
---
+++ Controllers/PatientController.cs
@@ -0,0 +1,26 @@
+﻿using iCareWebApplication.Data;
+using iCareWebApplication.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace iCareWebApplication.Controllers
+    {
+        public class PatientController : Controller // Ensure this is RoleController and it inherits from Controller
+        {
+            private readonly iCareContext _context;
+
+            public PatientController(iCareContext context)
+            {
+                _context = context;
+            }
+
+            // GET: /Role/Index
+            public async Task<IActionResult> Index()
+            {
+                var roles = await _context.Patient.ToListAsync();
+                return View(roles);
+            }
+        }
+    }
+
