c41a8debe33a2f1979f4fc051e91e261b9aba8cf
abhi pasupuleti <abhi25.pasupuleti@gmail.com>
2024-11-06 16:43:21 -0600

    created the logic to display users and well as hide links until user logs in.

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using iCareWebApplication.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace iCareWebApplication.Controllers
@@ -15,11 +16,48 @@ namespace iCareWebApplication.Controllers
             _context = context;
         }
 
-        // GET: /Role/Index
+        // GET: User/Index
         public async Task<IActionResult> Index()
         {
-            var roles = await _context.User.ToListAsync();
-            return View(roles);
+            var users = await _context.User.ToListAsync();
+            return View(users);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                user.DateCreate = DateTime.Now;
+                user.AccountStatus = "Active";
+                _context.Add(user);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // POST: User/DeleteSelected
+        [HttpPost]
+        public async Task<IActionResult> DeleteSelected(int[] selectedUserIds)
+        {
+            foreach (var userId in selectedUserIds)
+            {
+                var user = await _context.User.FindAsync(userId);
+                if (user != null)
+                {
+                    _context.User.Remove(user);
+                }
+            }
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
         }
     }
 }
