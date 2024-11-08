ac2d330d12c8a3c4aed6cf0660c713b77857f8c7
Aditya Ramakrishnan <112342054+aditya-ramki04@users.noreply.github.com>
2024-11-07 20:44:39 -0600

    longtext

---
+++ Migrations/20241108014954_FixICareModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace iCareWebApplication.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixICareModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
